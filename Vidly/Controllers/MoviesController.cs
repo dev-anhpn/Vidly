c2e93a99dacd9ae62be757fcc299aa7588aec70d
= <namanh9917@gmail.com>
2019-06-26 10:58:03 +0700

    Change 2

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -18,11 +18,34 @@ namespace Vidly.Controllers
             {
                 Name = "Tom And Jerry"
             };
-            //    return View(movie);
+            return View(movie);
             //    return Content("Hello Boss");
             //    return HttpNotFound();
             //    return  new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //    return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            // kiểm tra xem các thông tin có chỉ mục trang giá trị hay kh ông
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            // nếu là chuỗi hoặc không gian trắng
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";   
+            }
+
+            return Content(String.Format("pageIndex={0}$sortBy={1}",pageIndex,sortBy));
+        }
+
+
     }
 }
